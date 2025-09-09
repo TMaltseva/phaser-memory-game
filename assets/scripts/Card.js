be03c1ef1d37a22624ffd7353d541fe28eaa6c13
TMaltseva <bacardeonie@gmail.com>
2025-09-09 13:08:03 -0700

    feat: add tween animations to cards textures

--- assets/scripts/Card.js
+++ assets/scripts/Card.js
@@ -3,22 +3,41 @@ class Card extends Phaser.GameObjects.Sprite {
     super(scene, 0, 0, "card");
     this.scene = scene;
     this.value = value;
-    this.setOrigin(0, 0);
+    this.setOrigin(0.5, 0.5);
     this.scene.add.existing(this);
 
     this.setInteractive();
     this.opened = false;
-
     // this.on("pointerdown", this.open, this);
   }
 
   open() {
     this.opened = true;
-    this.setTexture("card" + this.value);
+    this.hide("card" + this.value);
   }
 
   close() {
     this.opened = false;
-    this.setTexture("card");
+    this.hide("card");
+  }
+
+  hide(texture) {
+    this.scene.tweens.add({
+      targets: this,
+      scaleX: 0,
+      ease: "Linear",
+      duration: 200,
+      onComplete: () => this.show(texture),
+    });
+  }
+
+  show(texture) {
+    this.setTexture(texture);
+    this.scene.tweens.add({
+      targets: this,
+      scaleX: 1,
+      ease: "Linear",
+      duration: 200,
+    });
   }
 }
